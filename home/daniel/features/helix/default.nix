1c18d95a5c57308d547af7c70f481329c6ae0460
danielalvsaaker <daniel.alvsaaker@protonmail.com>
2022-12-16 14:48:16 +0100

    Add theming to Helix

--- home/daniel/features/helix/default.nix
+++ home/daniel/features/helix/default.nix
@@ -2,8 +2,10 @@
   programs.helix = {
     enable = true;
     settings = {
+      theme = "fleet_dark";
       editor = {
         line-number = "relative";
+        true-color = true;
       };
     };
   };
