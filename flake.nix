59c2bf9ffc7788e35611036838e8048e97f577e8
Daniel Alvsåker <daniel.alvsaaker@protonmail.com>
2023-01-17 00:50:50 +0100

    Make firefox-addons follow main nixpkgs

--- flake.nix
+++ flake.nix
@@ -14,7 +14,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    firefox-addons.url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+     };
   };
 
   outputs =
