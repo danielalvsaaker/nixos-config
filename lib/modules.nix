84782a994a08c16f98ab4e93a04e4f341bf5609b
Danel Alvsåker <30574112+danielalvsaaker@users.noreply.github.com>
2023-11-12 00:52:21 +0100

    Simplify generateModules function

--- lib/modules.nix
+++ lib/modules.nix
@@ -1,26 +1,26 @@
 { lib, ... }:
-
-let
-  inherit (builtins) filter map toString listToAttrs;
-  inherit (lib) removeSuffix nameValuePair hasSuffix concatStringsSep removePrefix;
-  inherit (lib.filesystem) listFilesRecursive pathIsDirectory;
-  inherit (lib.attrsets) filterAttrs;
-
-  stripNixSuffix = path: removeSuffix ".nix" (removeSuffix "/default.nix" path);
-  cleanPathParts = path: filter (p: p != "") (lib.splitString "/" path);
-  makeModuleName = basePath: path:
-    let
-      relativePath = removePrefix (toString basePath + "/") (toString path);
-    in
-    concatStringsSep "-" (cleanPathParts (stripNixSuffix relativePath));
-in
 {
-  generateModules = basePath:
+  generateModules = path:
     let
-      filterNixFilesAndDirs = path: hasSuffix ".nix" (toString path) ||
-        (pathIsDirectory path && builtins.pathExists (toString path + "/default.nix"));
-      allNixFilesAndDirs = filter filterNixFilesAndDirs (listFilesRecursive basePath);
-      makeModulePair = path: nameValuePair (makeModuleName basePath path) (import path);
+      recurse = segments:
+        let
+          dir = builtins.concatStringsSep "/" ([ path ] ++ segments);
+          stat = builtins.readDir dir;
+          f = acc: name:
+            if stat.${name} == "directory" then
+              acc // recurse (segments ++ [ name ])
+            else if name == "default.nix" && segments != [ ] then
+              acc // {
+                "${builtins.concatStringsSep "-" segments}" = dir;
+              }
+            else if lib.hasSuffix ".nix" name then
+              acc // {
+                "${builtins.concatStringsSep "-" (segments ++ [ (lib.removeSuffix ".nix" name) ])}" = (dir + "/" + name);
+              }
+            else
+              acc;
+        in
+        builtins.foldl' f { } (builtins.attrNames stat);
     in
-    listToAttrs (filter ({ name, value }: name != "default") (map makeModulePair allNixFilesAndDirs));
+    recurse [ ];
 }
