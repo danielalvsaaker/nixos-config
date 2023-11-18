c6d29b05f1ffe851a4de5ef7f1eeb053afeb4e4a
danielalvsaaker <30574112+danielalvsaaker@users.noreply.github.com>
2023-11-18 12:34:17 +0100

    Set up automatic GC/optimising of store

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,4 +1,17 @@
 {
   nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
+
+    optimise = {
+      dates = [ "weekly" ];
+      automatic = true;
+    };
+
+    gc = {
+      dates = "weekly";
+      automatic = true;
+      options = "--delete-older-than 30d";
+    };
+  };
 }
