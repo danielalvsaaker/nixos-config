eea234cf7d68d6ba38642c70d936a6e6cfd5262c
danielalvsaaker <30574112+danielalvsaaker@users.noreply.github.com>
2024-05-30 15:03:35 +0200

    Add temporary workaround for ASCII issue with .NET SDK

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -5,10 +5,21 @@ let
 in
 {
   flake.nixosModules = modules // {
-    default = {
+    default = { lib, ... }: {
       imports = [
         config.flake.nixosModules.font
         config.flake.nixosModules.nix
+
+        # https://github.com/NixOS/nixpkgs/issues/315574
+        ({ lib, options, ... }: {
+          options.system.nixos.codeName = lib.mkOption { readOnly = false; };
+          config.system.nixos.codeName =
+            let
+              codeName = options.system.nixos.codeName.default;
+              renames."Vicuña" = "Vicuna";
+            in
+              renames."${codeName}" or (throw "Unknown `codeName`: ${codeName}.");
+        })
       ];
     };
   };
