e8e5002909bca6ea5010c3ecb1191b0fffc6736f
danielalvsaaker <30574112+danielalvsaaker@users.noreply.github.com>
2024-06-17 10:25:54 +0200

    feat(modules/nixos) remove workaround for UTF-8 issue

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -9,17 +9,6 @@ in
       imports = [
         config.flake.nixosModules.font
         config.flake.nixosModules.nix
-
-        # https://github.com/NixOS/nixpkgs/issues/315574
-        ({ lib, options, ... }: {
-          options.system.nixos.codeName = lib.mkOption { readOnly = false; };
-          config.system.nixos.codeName =
-            let
-              codeName = options.system.nixos.codeName.default;
-              renames."Vicuña" = "Vicuna";
-            in
-              renames."${codeName}" or (throw "Unknown `codeName`: ${codeName}.");
-        })
       ];
     };
   };
