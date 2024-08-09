736423c3ab89f6004ecdaea32e67d4f7f05af8d9
danielalvsaaker <30574112+danielalvsaaker@users.noreply.github.com>
2024-08-09 11:15:56 +0200

    feat(modules/warp): use new module options defined in nixpkgs

--- modules/nixos/cloudflare-warp.nix
+++ modules/nixos/cloudflare-warp.nix
@@ -1,14 +1,5 @@
-{ pkgs, ... }:
 {
-  systemd.packages = [ pkgs.cloudflare-warp ];
-  systemd.targets.multi-user.wants = [ "warp-svc.service" ];
-
-  systemd.user.services.warp-taskbar.serviceConfig = {
-    TemporaryFileSystem = "/usr:ro";
-    BindReadOnlyPaths = [
-      "${pkgs.cloudflare-warp}/share/:/usr/share/"
-    ];
-  };
+  services.cloudflare-warp.enable = true;
 
   services.resolved.extraConfig = ''
     ResolveUnicastSingleLabel=yes
